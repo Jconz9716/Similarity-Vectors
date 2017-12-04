9417030e03eef9e8e927a2580d43fafe068fc554
Jared <jared.hammer@gmail.com>
2017-12-04 11:26:03 -0600

    create test package

---
+++ src/TestPackages/Test1.java
@@ -0,0 +1,32 @@
+
+package TestPackages;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Test1 {
+    
+    public Part1Test() {
+    }
+                // can rename tests as needed
+		@Test
+		public void testA() {
+                    assertEquals(true, true);
+		}
+		
+		@Test
+		public void testB() {
+
+		}
+		
+		@Test
+		public void testC() {
+
+		}
+		
+		
+
+
+		
+    
+}
