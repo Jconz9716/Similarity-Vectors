ce74598b8354dab82780810e97682d347bb75fb0
Jared <jared.hammer@gmail.com>
2017-12-04 12:57:38 -0600

    create test package

--- Tests/Part2Test.java
+++ Tests/Part2Test.java
@@ -1,2 +1,22 @@
+
+import edu.uiowa.cs.similarity.Vector;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.io.*;
+
+import static org.junit.Assert.*;
+
 public class Part2Test {
+        public Part2Test() {
+    }
+        
+        @Test
+        public void vectorTest() {
+            // need to fill in
+            assertFalse("Implement test", true);
+        }
+        
+        
 }
